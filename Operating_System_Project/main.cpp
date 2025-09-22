bb36f9bfeadab62e934c17656566f545c0afe2c8
eddiet303 <eddietorres2468@gmail.com>
2025-09-21 23:13:04 -0500

    Updated main

--- Operating_System_Project/main.cpp
+++ Operating_System_Project/main.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main() {
 
+	string username, password;
+	
 	cout << "OS is booting up..." << endl;
+	cout << "Enter username: ";
+	cin >> username;
+	cout << endl;
+	cout << "Enter password: ";
+	cin >> password;
+	cout << endl;
 
 	return 0;
 }
