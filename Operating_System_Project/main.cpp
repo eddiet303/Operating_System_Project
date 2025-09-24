b159d640f47862e056e325b97b42265654f0538a
SrStop <jose_santos@dusty.tamiu.edu>
2025-09-24 01:50:36 -0500

    Update main.cpp

--- Operating_System_Project/main.cpp
+++ Operating_System_Project/main.cpp
@@ -6,13 +6,20 @@ using namespace std;
 
 int main() {
 
+	//Variables for user input
 	string username, password;
 	cout << "OS is booting up..." << endl;
-
+	
+	//Track the numer of login attempts
 	int attempts = 0;
 	const int maxAttempts = 3;
 
+	//loop to chack the values of string or max attemts Reached
 	while (attempts < maxAttempts){
+/*here the user input is store one time compare the user input if the input
+is not the same as the authentication credential then the user overwrite the 
+value of the username again until the credential match
+		*/
 	cout << "Enter username: ";
 	cin >> username;
 	cout << endl;
@@ -20,6 +27,7 @@ int main() {
 	cin >> password;
 	cout << endl;
 
+//call the authentication function
 	if (authenticateUser(username, password)) {
 		break;
 	}
