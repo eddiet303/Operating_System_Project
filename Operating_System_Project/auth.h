ad5906e63a210e224fefec0a0a2adbf8b2e88aaa
eddiet303 <eddietorres2468@gmail.com>
2025-10-16 14:02:13 -0500

    updated auth.ccp/auth.h/main.cpp

--- Operating_System_Project/auth.h
+++ Operating_System_Project/auth.h
@@ -1,1 +1,12 @@
+/*
+auth. h declares the function authenticateUser
+*/
+
 #pragma once
+#ifndef AUTH_H
+#define AUTH_H
+#include<string>
+
+bool authenticateUser(const std::string validUsername, const std::string validPassword);
+
+#endif
